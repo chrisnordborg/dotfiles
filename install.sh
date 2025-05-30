83108b5bfebda022b23715eb7a6d084f9cbc5965
Christian Nordborg <nordborgchristian@gmail.com>
2025-05-30 23:45:54 +0200

    Started chaning install.sh to be distro-independent.

--- install.sh
+++ install.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
 # Custom fresh install of a new system
-# Version: 1.0
-# Date: 2025-04-26
-# Description: 
+# Version: 1.2
+# Date: 2025-05-30
+# Description: This is my personal installation script for a clean OS install. Currently reworking it to be distro-independet.
 # Author: Christian Nordborg
-# TODO: move scripts / create links to the correct places.
-#	handle bluetooth-dongle
-# Adding a new comment for testing.
+
+
+
+# Initializing which package manager to used based on your system.
+OS = $(hostnamectl | grep "Operating System")
+
+if [[ $OS == *"Ubuntu"* ]]; then
+# Use -y at the end to answer Yes to all the prompts, needing only to answer yes once.
+    PM="apt install -y"
+    XTRA="snap install -y"
+
+elif [[ $OS == *"Arch"* ]]; then
+    PM="pacman --noconfirm"
+
+else
+    echo "Unable to determine your Linux distribution. Cancelling the installation process."
+    exit 1
+fi
+
 
 sudo apt update
-# Use -y at the end to answer Yes to all the prompts, needing only to answer yes once.
 
 # Function to install basic utilities
 install_basic() {
     echo "Installing basic utilities..."
-    sudo apt install -y libreoffice timeshift rhythmbox gimp 
+    sudo $PM libreoffice timeshift rhythmbox gimp 
     
-    dpkg -s brave &> /dev/null && echo "Brave is already installed" || sudo snap install brave
+    dpkg -s brave &> /dev/null && echo "Brave is already installed" || sudo XTRA brave
     # Create an autostart file to start brave on startup.
     echo "[Desktop Entry]" > ~/.config/autostart/brave.desktop
     echo "Type=Application" >> ~/.config/autostart/brave.desktop
@@ -30,7 +45,7 @@ install_basic() {
     echo "Comment[en_US]=" >> ~/.config/autostart/brave.desktop
     echo "Comment=" >> ~/.config/autostart/brave.desktop
 
-    dpkg -s spotify &> /dev/null && echo "Spotify is already installed" || sudo snap install spotify
+    dpkg -s spotify &> /dev/null && echo "Spotify is already installed" || sudo XTRA spotify
 
 }
 
@@ -40,11 +55,11 @@ install_basic() {
 # Function to install gaming utilities
 install_gaming() {
     echo "Installing gaming utilities..."
-    sudo apt install -y lutris wine proton
-    sudo snap install steam
+    sudo $PM wine proton
+    sudo XTRA steam
     sudo dpkg --add-architecture i386
-sudo apt update
-sudo apt install libnvidia-gl-550:i386
+    sudo apt update
+    sudo $PM libnvidia-gl-550:i386
     
     sudo add-apt-repository multiverse
     sudo apt update
@@ -53,9 +68,8 @@ sudo apt install libnvidia-gl-550:i386
     ##steam
     sudo add-apt-repository ppa:lutris-team/lutris
     sudo apt update
-    sudo apt install lutris
-    sudo apt install python3-yaml python3-requests python3-pil python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-gnomedesktop-3.0 gir1.2-webkit2-4.0 gir1.2-notify-0.7 psmisc cabextract unzip p7zip curl fluid-soundfont-gs x11-xserver-utils python3-evdev libgirepository1.0-dev python3-setproctitle python3-distro
-    sudo apt install wine
+    sudo $PM lutris
+    sudo $PM python3-yaml python3-requests python3-pil python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-gnomedesktop-3.0 gir1.2-webkit2-4.0 gir1.2-notify-0.7 psmisc cabextract unzip p7zip curl fluid-soundfont-gs x11-xserver-utils python3-evdev libgirepository1.0-dev python3-setproctitle python3-distro
 }
 
 
@@ -67,8 +81,8 @@ sudo apt install libnvidia-gl-550:i386
 install_developer() {
     echo "Installing developer applications..."
 
-    mkdir ~/dotfiles
-    sudo apt install stow
+    mkdir -p ~/dotfiles
+    sudo $PM stow
     
     # Create autostart file to start terminal on startup.
     echo "[Desktop Entry]" > ~/.config/autostart/gnome-terminal.desktop
@@ -100,12 +114,12 @@ install_developer() {
     cd dotfiles
     git remote set-url origin git@github.com:chrisnordborg/dotfiles.git
     stow .
-    sudo apt install -y git tmux zsh neovim
-    dpkg -s intellij-idea-community &> /dev/null && echo "Intellij is already installed" || sudo snap install intellij-idea-community --classic
-    #sudo snap install intellij-idea-community --classic
+    sudo $PM git tmux neovim
+    dpkg -s intellij-idea-community &> /dev/null && echo "Intellij is already installed" || sudo XTRA intellij-idea-community --classic
+    #sudo XTRA intellij-idea-community --classic
     
     #ZSH
-    sudo apt install -y zsh
+    sudo $PM zsh
     sudo chsh -s /usr/bin/zsh
     
     #OhMyZSH
@@ -114,17 +128,17 @@ install_developer() {
     # NeoVim extras
     sudo add-apt-repository ppa:neovim-ppa/unstable -y
     sudo apt update
-    sudo apt install make gcc ripgrep unzip git xclip neovim
+    sudo $PM make gcc ripgrep unzip git xclip neovim
     
     # kitty terminal
-    sudo apt install kitty
+    sudo $PM kitty
     #sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty 50
     sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/bin/kitty
     sudo update-alternatives --config x-terminal-emulator
 
     # i3
     sudo apt-get install -y i3 feh
-    sudo apt install -y polybar fonts-font-awesome
+    sudo $PM polybar fonts-font-awesome
     # Make scripts executable.
     chmod +x ~/.config/polybar/launch.sh
     chmod +x ~/.config/polybar/scripts/battery.sh
@@ -134,31 +148,31 @@ install_developer() {
     chmod +x ~/.config/polybar/scripts/date.sh
     chmod +x ~/.config/polybar/scripts/memory.sh
     # This is to auto-disable NUM LOCK upon startup.
-    sudo apt install -y xdotool
+    sudo $PM xdotool
 
     # Extract DevaVuSansM Nerd Font
     unzip ~/dotfiles/DejaVuSansMono.zip -d /usr/share/fonts/
 
     # picom for transparent windows and terminal
-    sudo apt install -y picom libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev cmake
+    sudo $PM picom libconfig-dev libdbus-1-dev libegl-dev libev-dev libgl-dev libepoxy-dev libpcre2-dev libpixman-1-dev libx11-xcb-dev libxcb1-dev libxcb-composite0-dev libxcb-damage0-dev libxcb-glx0-dev libxcb-image0-dev libxcb-present-dev libxcb-randr0-dev libxcb-render0-dev libxcb-render-util0-dev libxcb-shape0-dev libxcb-util-dev libxcb-xfixes0-dev meson ninja-build uthash-dev cmake
     meson setup --buildtype=release build
     ninja -C build
     ninja -C build install
 
     #VScode
-    sudo snap install --classic code
-    sudo apt install -y software-properties-common apt-transport-https wget
+    sudo XTRA --classic code
+    sudo $PM software-properties-common apt-transport-https wget
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
     sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
     sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo snap install -y code
+    sudo XTRA -y code
 
     # Kotlin
     # Build the language server and install it in ./server/build/install/server.
     git clone https://github.com/fwcd/kotlin-language-server.git
     cd kotlin-language-server
     # The Kotlin language server requires Java 11.
-    sudo apt install -y openjdk-11-jdk
+    sudo $PM openjdk-11-jdk
     export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
     export PATH="$JAVA_HOME/bin:$PATH"
     
@@ -186,12 +200,12 @@ install_personal() {
     # Qbittorrent
     sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
     sudo apt update
-    sudo apt install qbittorrent
+    sudo $PM qbittorrent
     
     # Discord
-    dpkg -s discord &> /dev/null && echo "Discord is already installed" || sudo snap install discord
-    #dpkg -s obsidian &> /dev/null && echo "Obsidian is already installed" || sudo snap install obsidian
-    sudo snap install obsidian --classic
+    dpkg -s discord &> /dev/null && echo "Discord is already installed" || sudo XTRA discord
+    #dpkg -s obsidian &> /dev/null && echo "Obsidian is already installed" || sudo XTRA obsidian
+    sudo XTRA obsidian --classic
     chmod a+x ~/dotfiles/obsidian-1.8.10.AppImage
     ~/dotfiles/obsidian-1.8.10.AppImage
     
@@ -200,11 +214,7 @@ install_personal() {
     sudo apt-get install pulseaudio-utils    
 
     #Obsidian - OneDrive sync
-    sudo apt install build-essential
-    sudo apt install libcurl4-openssl-dev
-    sudo apt install libsqlite3-dev
-    sudo apt install pkg-config
-    sudo apt install git
+    sudo $PM build-essential libcurl4-openssl-dev libsqlite3-dev pkg-config git
     cd ~/Downloads
     sudo wget https://netcologne.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
     sudo apt-get update --allow-insecure-repositories
