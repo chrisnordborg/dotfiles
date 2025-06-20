5cf91f33699ff3149b7bbdcf2b3d5827afef8a18
Christian Nordborg <nordborgchristian@gmail.com>
2025-06-20 16:19:12 +0200

    Started work on concentration conversion script.

--- .config/scripts/convert_concentration.sh
+++ .config/scripts/convert_concentration.sh
@@ -1,33 +1,29 @@
 #!/bin/sh
 
+mg_to_mmol (conc)  {
+}
+
 command -v bc >/dev/null || { notify-send "Error: bc not found"; exit 1; }
 
-c_to_f() {
-    celsius="$(echo "" | $menuB --prompt "Enter temperature in Celsius: " <&-)"
-    [ -z "$celsius" ] && exit
-    [[ "$celsius" =~ ^-?[0-9]+(\.[0-9]+)?$ ]] || { notify-send "Error: Not a valid number"; exit 1; }
-    fahrenheit=$(bc <<< "scale=2; ($celsius * 9/5) + 32")
-    notify-send  "$celsius°C is equal to $fahrenheit°F"
-    #notify-send -h string:bgcolor:#bf616a "$celsius°C is equal to $fahrenheit°F"
-}
+menu="tofi -c $HOME/.config/tofi/configA --height 80 --width 400 --require-match=false"
+temp=$(echo "" | $menu --prompt "Enter a concentration (mmol/L or mg/dL): " )
+# Molar masses (g/mol)
+cholesterol=386.654
+glucose=180.156
 
-f_to_c() {
-    fahrenheit="$(echo "" | $menuB --prompt "Enter temperature in Fahrenheit: " <&-)"
-    [ -z "$fahrenheit" ] && exit
-    [[ "$fahrenheit" =~ ^-?[0-9]+(\.[0-9]+)?$ ]] || { notify-send "Error: Not a valid number"; exit 1; }
-    celsius=$(bc <<< "scale=2; ($fahrenheit - 32) * 5/9")
-    notify-send "$fahrenheit°F is equal to $celsius°C"
-    #notify-send -h string:bgcolor:#81a1c1 "$fahrenheit°F is equal to $celsius°C"
-}
 
-menu="tofi -c $HOME/.config/tofi/configA --height 125 --width 400 --require-match=false"
-menuB="tofi -c $HOME/.config/tofi/configA --height 60 --width 450 --require-match=false"
-choose=$(printf "%s\nCelcius to Fahrenheit\nFahrenheit to Celcius" | $menu --prompt "Choose: " )
+[ -z "$temp" ] && exit
+[[ "$temp" =~ ^-?[0-9]+(\.[0-9]+)?$ ]] || { notify-send "Error: Not a valid number"; exit 1; }
+#cholesterol
+cholesterol_mg_to_mmol=$(bc <<< "scale=2; ($temp * $cholesterol / 10)")
+cholesterol_mmol_to_mg=$(bc <<< "scale=2; ($temp / $cholesterol * 10)")
 
-[ -z "$choose" ] && exit
-case "$choose" in
-    *Fahrenheit) c_to_f ;;
-    *Celcius) f_to_c ;;
-    *) exit ;;
-esac
+glucose_mg_to_mmol
 
+#glucose
+
+notify-send -t 5000 -h string:bgcolor:#916B4A \
+	"$(echo -e \
+"Cholesterol:\n\
+$temp mmol/L is equal to:   $cholesterol_mg_to_mmol mg/dL\n\
+$temp mg/dL is equal to:   $cholesterol_mmol_to_mg mmol/L")"
