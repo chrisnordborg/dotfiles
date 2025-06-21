cb3b5b68e3a85f8bead4bb63d9404200f4319591
Christian Nordborg <nordborgchristian@gmail.com>
2025-06-21 23:16:45 +0200

    Optimized note script

--- .config/scripts/create_note.sh
+++ .config/scripts/create_note.sh
@@ -8,7 +8,7 @@ timestamp=$(date +%F_%T | tr ':' '-') # e.g. 2025-06-21_21-24-00
 
 # Create a new note
 newnote() {
-  name=$($menu --height 80 --width 500 --require-match=false --prompt 'New note: ' <<< "" 2>/dev/null)
+  name=$($menu --height 40 --width 500 --require-match=false --prompt 'New note: ' <<< "" 2>/dev/null)
   [ -z "$name" ] && name="$timestamp"
   filepath="$newNoteFolder$name.md"
   "$terminal" nvim "$filepath" >/dev/null 2>&1
@@ -16,14 +16,21 @@ newnote() {
 
 selected() {
   # Map of filename -> full path (may include duplicates!)
-  mapfile -t all_files < <(find "$mainFolder" -type f -name "*.md")
-  filenames=$(printf "%s\n" "${all_files[@]##*/}" | sort -u)
+  # find ... -printf "%T@ %p\n": prints epoch_time full/path/to/file.
+  # sort -nr: sorts numerically and reverses, so newest is first.
+  # cut -d' ' -f2-: strips the timestamp, keeping only the path.
+  mapfile -t all_files < <(
+  find "$mainFolder" -type f -name "*.md" -printf "%T@ %p\n" |
+  sort -k1,1nr |
+  cut -d' ' -f2-
+  )
+  filenames=$(printf "%s\n" "${all_files[@]##*/}")
 
-  choice=$(echo -e "[New Note]\n$filenames" | $menu --width 900 --prompt "Choose note: ")
+  choice=$(echo -e "[New Note]\n$filenames" | $menu --height 600 --width 900 --num-results 0 --prompt "Choose note: ")
   [ $? -ne 0 ] && exit 1
 
   case "$choice" in
-    New)
+    "[New Note]")
       newnote
       ;;
     *.md)
