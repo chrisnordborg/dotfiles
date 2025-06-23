6696b3b11a660c1f87b561c9d3e230d0ba8b058f
Christian Nordborg <nordborgchristian@gmail.com>
2025-06-23 22:13:00 +0200

    Added bluetooth restart to scripts

--- .config/scripts/audio_output_switch.sh
+++ .config/scripts/audio_output_switch.sh
@@ -1,26 +1,28 @@
 #!/bin/sh
 
 options=$(pactl -f json list sinks | jq -r '.[] | .description')
+scanbt="[Scan for bluetooth headset]"
+togglebt="$HOME/.config/scripts/toggle_bluetooth_headsets_OnOff.sh"
+restartbt="[Restart Bluetooth]"
 
 # Case-insensitive matching enabled
 shopt -s nocasematch
 
-scanbt="[Scan for bluetooth headset]"
-togglebt="$HOME/.config/scripts/toggle_bluetooth_headsets_OnOff.sh"
-
-selection=$(echo -e "$options\n$scanbt" | tofi -c ~/.config/tofi/configA --prompt "Set audio output: ")
+selection=$(echo -e "$options\n$scanbt\n$restartbt" | tofi -c ~/.config/tofi/configA --prompt "Set audio output: ")
 clean_selection=$(printf "%s" "$selection" | tr -d '\n\r')
 
-if [ "$1" = "--reset" ]; then
+if [ "$1" = "$restartbt" ]; then
     echo "Resetting Bluetooth..."
     sudo systemctl restart bluetooth
     sleep 2
+    exit
 fi
 
 # If user chose to scan for Bluetooth headset
 if [ "$clean_selection" = "$scanbt" ]; then
     # Run Bluetooth toggle script, capture lines that start with "Connected:" or "Disconnected:"
-    output=$(bash ~/.config/scripts/toggle_bluetooth_headsets_OnOff.sh | grep -E '^Connected:|^Disconnected:')
+	notify-send "begin"
+    output=$(bash $togglebt | grep -E '^Connected:|^Disconnected:')
 
     # If output is non-empty, show notification
     [ -n "$output" ] && notify-send "Audio: $output"
